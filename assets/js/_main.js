a0d481902785d1b2e260b603953307a2d116a0c0
ZheyuHarry <1035168426@qq.com>
2026-09-10 10:10:19 +0800

    Fix sidebar rules hiding Wechat contact button

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -27,8 +27,8 @@ $(document).ready(function(){
   $(".sticky").Stickyfill();
 
   var stickySideBar = function(){
-    var show = $(".author__urls-wrapper button").length === 0 ? $(window).width() > 925 : !$(".author__urls-wrapper button").is(":visible");
-    // console.log("has button: " + $(".author__urls-wrapper button").length === 0);
+    var show = $(".author__urls-wrapper > button").length === 0 ? $(window).width() > 925 : !$(".author__urls-wrapper > button").is(":visible");
+    // console.log("has button: " + $(".author__urls-wrapper > button").length === 0);
     // console.log("Window Width: " + windowWidth);
     // console.log("show: " + show);
     //old code was if($(window).width() > 1024)
@@ -52,9 +52,9 @@ $(document).ready(function(){
 
   // Follow menu drop down
 
-  $(".author__urls-wrapper button").on("click", function() {
+  $(".author__urls-wrapper > button").on("click", function() {
     $(".author__urls").fadeToggle("fast", function() {});
-    $(".author__urls-wrapper button").toggleClass("open");
+    $(".author__urls-wrapper > button").toggleClass("open");
   });
 
   // init smooth scroll
